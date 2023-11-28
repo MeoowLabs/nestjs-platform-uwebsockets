3e7319641e5c803650131d04f9e3a6753a972814
Adrianmjim <adrianmjim199434@gmail.com>
2023-11-28 23:29:16 +0100

    wip

--- src/builder/ExpressLikeRequestFromUWebSocketsHttpRequestBuilder.ts
+++ src/builder/ExpressLikeRequestFromUWebSocketsHttpRequestBuilder.ts
@@ -7,27 +7,27 @@ export class ExpressLikeRequestFromUWebSocketsHttpRequestBuilder
   implements Builder<ExpressLikeRequest, [HttpRequest, HttpResponse, string]>
 {
   readonly #expressLikeBodyFromUWebSocketsHttpBodyBuilder: Builder<ExpressLikeBody, [HttpResponse]>;
-  readonly #expressLikeParamsFromUWebSocketsHttpParamsBuilder: Builder<ExpressLikeParams, [HttpRequest, string]>;
   readonly #expressLikeHeadersFromUWebSocketsHttpHeadersBuilder: Builder<ExpressLikeHeaders, [HttpRequest]>;
+  readonly #expressLikeParamsFromUWebSocketsHttpParamsBuilder: Builder<ExpressLikeParams, [HttpRequest, string]>;
   readonly #expressLikeQueryFromUWebSocketsHttpQueryBuilder: Builder<ExpressLikeQuery, [HttpRequest]>;
 
   private constructor(
     readonly expressLikeBodyFromUWebSocketsHttpBodyBuilder: Builder<ExpressLikeBody, [HttpResponse]>,
-    readonly expressLikeParamsFromUWebSocketsHttpParamsBuilder: Builder<ExpressLikeParams, [HttpRequest, string]>,
     readonly expressLikeHeadersFromUWebSocketsHttpHeadersBuilder: Builder<ExpressLikeHeaders, [HttpRequest]>,
+    readonly expressLikeParamsFromUWebSocketsHttpParamsBuilder: Builder<ExpressLikeParams, [HttpRequest, string]>,
     readonly expressLikeQueryFromUWebSocketsHttpQueryBuilder: Builder<ExpressLikeQuery, [HttpRequest]>,
   ) {
     this.#expressLikeBodyFromUWebSocketsHttpBodyBuilder = expressLikeBodyFromUWebSocketsHttpBodyBuilder;
-    this.#expressLikeParamsFromUWebSocketsHttpParamsBuilder = expressLikeParamsFromUWebSocketsHttpParamsBuilder;
     this.#expressLikeHeadersFromUWebSocketsHttpHeadersBuilder = expressLikeHeadersFromUWebSocketsHttpHeadersBuilder;
+    this.#expressLikeParamsFromUWebSocketsHttpParamsBuilder = expressLikeParamsFromUWebSocketsHttpParamsBuilder;
     this.#expressLikeQueryFromUWebSocketsHttpQueryBuilder = expressLikeQueryFromUWebSocketsHttpQueryBuilder;
   }
 
 
   public build(request: HttpRequest, response: HttpResponse, path: string): ExpressLikeRequest {
-    const params: ExpressLikeParams = this.#expressLikeParamsFromUWebSocketsHttpParamsBuilder.build(request, path);
     const body: ExpressLikeBody = this.#expressLikeBodyFromUWebSocketsHttpBodyBuilder.build(response);
     const headers: ExpressLikeHeaders = this.#expressLikeHeadersFromUWebSocketsHttpHeadersBuilder.build(request);
+    const params: ExpressLikeParams = this.#expressLikeParamsFromUWebSocketsHttpParamsBuilder.build(request, path);
     const query: ExpressLikeQuery = this.#expressLikeQueryFromUWebSocketsHttpQueryBuilder.build(request);
 
     const expressLikeRequest: ExpressLikeRequest = {
